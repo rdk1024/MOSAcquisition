f6fcd8e7759e060f462a6bafc261ba06cd488113
Justin Kunimune <justinku@castor.subaru.nao.ac.jp>
2016-07-05 17:01:10 -1000

    Renamed MOSAcquisition to MESOffset1, to make room for MESOffset 2 and 3

--- MESOffset1.py
+++ MESOffset1.py
@@ -14,7 +14,7 @@ from ginga.gw import Widgets, Viewers
 # third-party imports
 import astroplan
 
-class MOSAcquisition(GingaPlugin.LocalPlugin):
+class MESOffset1(GingaPlugin.LocalPlugin):
 
     def __init__(self, fv, fitsimage):
         """
@@ -26,7 +26,7 @@ class MOSAcquisition(GingaPlugin.LocalPlugin):
             on which the plugin is being invoked
         """
         # superclass constructor defines self.fv, self.fitsimage, and self.logger
-        super(MOSAcquisition, self).__init__(fv, fitsimage)
+        super(MESOffset1, self).__init__(fv, fitsimage)
 
         # initialize some constants
         self.title_font = self.fv.getFont("sansFont", 18)
@@ -35,6 +35,7 @@ class MOSAcquisition(GingaPlugin.LocalPlugin):
         self.colors = ('green','red','blue','yellow','magenta','cyan','orange')
 
         # and some attributes
+        self.step = 1
         self.click_history = []
         self.click_index = -1
 
@@ -140,7 +141,8 @@ class MOSAcquisition(GingaPlugin.LocalPlugin):
                               x+self.sq_size+dx, y+self.sq_size+dy)
             cropped_data = src_image.cutout_adjust(x1,y1,x2,y2)[0]
             viewer.set_data(cropped_data)
-            self.fitsimage.copy_attributes(viewer, ['transforms','cutlevels','rgbmap'])
+            self.fitsimage.copy_attributes(viewer,
+                                           ['transforms','cutlevels','rgbmap'])
             
         self.canvas.enable_draw(False)
 
@@ -155,77 +157,79 @@ class MOSAcquisition(GingaPlugin.LocalPlugin):
         out = Widgets.VBox()
         out.set_border_width(4)
         container.add_widget(out)
-        
-        # now create the VBox to hold the majority of the commands,
-        # the ScrollWidget that contains that box,
-        # and a string to specify whether this is horizontal or vertical
-        gui, gui_wrapper, orientation = Widgets.get_oriented_box(container)
-        gui.set_border_width(4)
-        gui.set_spacing(3)
-        out.add_widget(gui_wrapper)
-        
-        # create a label to title this step
-        lbl = Widgets.Label("Step 1")
-        lbl.set_font(self.title_font)
-        gui.add_widget(lbl)
+            
+        # the rest depends on which step we are on
+        if self.step == 1:
+            # now create the VBox to hold the majority of the commands,
+            # the ScrollWidget that contains that box,
+            # and a string to specify whether this is horizontal or vertical
+            gui, gui_wrapper, orientation = Widgets.get_oriented_box(container)
+            gui.set_border_width(4)
+            gui.set_spacing(3)
+            out.add_widget(gui_wrapper)
+            
+            # create a label to title this step
+            lbl = Widgets.Label("Step 1")
+            lbl.set_font(self.title_font)
+            gui.add_widget(lbl)
 
-        # fill a text box with brief instructions and put in in an expander
-        exp = Widgets.Expander(title="Instructions")
-        gui.add_widget(exp)
-        txt = Widgets.TextArea(wrap=True, editable=False)
-        txt.set_font(self.body_font)
-        txt.set_text("Left click on the star labeled '#1'. The other stars should appear in the boxes below. Click again to select another position. Click 'Next' when you are satisfied with your location.")
-        exp.set_widget(txt)
+            # fill a text box with brief instructions and put in in an expander
+            exp = Widgets.Expander(title="Instructions")
+            gui.add_widget(exp)
+            txt = Widgets.TextArea(wrap=True, editable=False)
+            txt.set_font(self.body_font)
+            txt.set_text("Left click on the star labeled '#1'. The other stars should appear in the boxes below. Click again to select another position. Click 'Next' when you are satisfied with your location.")
+            exp.set_widget(txt)
 
-        # create a box to group the controls together
-        box = Widgets.HBox()
-        box.set_spacing(3)
-        gui.add_widget(box)
+            # create a box to group the controls together
+            box = Widgets.HBox()
+            box.set_spacing(3)
+            gui.add_widget(box)
 
-        # the undo button goes back a step
-        btn = Widgets.Button("Undo")
-        btn.add_callback('activated', lambda w: self.undo_cb())
-        btn.set_tooltip("Undo a single click (if a click took place)")
-        box.add_widget(btn)
+            # the undo button goes back a step
+            btn = Widgets.Button("Undo")
+            btn.add_callback('activated', lambda w: self.undo_cb())
+            btn.set_tooltip("Undo a single click (if a click took place)")
+            box.add_widget(btn)
 
-        # the redo button goes forward
-        btn = Widgets.Button("Redo")
-        btn.add_callback('activated', lambda w: self.redo_cb())
-        btn.set_tooltip("Undo an undo action (if an undo action took place)")
-        box.add_widget(btn)
+            # the redo button goes forward
+            btn = Widgets.Button("Redo")
+            btn.add_callback('activated', lambda w: self.redo_cb())
+            btn.set_tooltip("Undo an undo action (if an undo action took place)")
+            box.add_widget(btn)
 
-        # the clear button erases the canvas
-        btn = Widgets.Button("Clear")
-        btn.add_callback('activated', lambda w: self.canvas.delete_all_objects())
-        btn.set_tooltip("Erase all marks on the canvas")
-        box.add_widget(btn)
-        
-        # the next button moves on to step 2
-        btn = Widgets.Button("Next")
-        btn.add_callback('activated', lambda w: self.close())
-        btn.set_tooltip("Accept and proceed to step 2")
-        box.add_widget(btn)
-        
-        # lastly, we need the zoomed-in images. This is the grid we put them in
-        num_img = len(self.star_list)   # total number of alignment stars
-        columns = 2                       # pictures in each row
-        rows = int(math.ceil(float(num_img)/columns))
-        grd = Widgets.GridBox(rows=rows, columns=columns)
-        gui.add_widget(grd)
-        
-        # these are the images we put in the grid
-        for row in range(0, rows):
-            for col in range(0, columns):
-                i = row*columns + col
-                if i < num_img:
-                    pic = Viewers.GingaViewerWidget(viewer=self.thumbnails[i])
-                    grd.add_widget(pic, row, col)
+            # the clear button erases the canvas
+            btn = Widgets.Button("Clear")
+            btn.add_callback('activated', lambda w: self.canvas.delete_all_objects())
+            btn.set_tooltip("Erase all marks on the canvas")
+            box.add_widget(btn)
+            
+            # the next button moves on to step 2
+            btn = Widgets.Button("Next")
+            btn.add_callback('activated', lambda w: self.close())
+            btn.set_tooltip("Accept and proceed to step 2")
+            box.add_widget(btn)
+            
+            # lastly, we need the zoomed-in images. This is the grid we put them in
+            num_img = len(self.star_list)   # total number of alignment stars
+            columns = 2                       # pictures in each row
+            rows = int(math.ceil(float(num_img)/columns))
+            grd = Widgets.GridBox(rows=rows, columns=columns)
+            gui.add_widget(grd)
+            
+            # these are the images we put in the grid
+            for row in range(0, rows):
+                for col in range(0, columns):
+                    i = row*columns + col
+                    if i < num_img:
+                        pic = Viewers.GingaViewerWidget(viewer=self.thumbnails[i])
+                        grd.add_widget(pic, row, col)
 
         # end is an HBox that comes at the very end, after the rest of the gui
         end = Widgets.HBox()
         end.set_spacing(3)
         out.add_widget(end)
-        
+            
         # throw in a close button at the very end, just in case
         btn = Widgets.Button("Close")
         btn.add_callback('activated', lambda w: self.close())
@@ -288,15 +292,11 @@ class MOSAcquisition(GingaPlugin.LocalPlugin):
         """
         Called whenever a new image is loaded
         """
-       # for viewer in self.thumbnails:
-       #     self.fitsimage.copy_attributes(viewer,
-       #                                    ['transforms', 'cutlevels',
-       #                                    'rgbmap'])
         pass
 
 
     def __str__(self):
-        return "MOSAcquisition"
+        return "MESOffset1"
         
         
 
