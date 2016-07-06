3558c03a416725e7af51962c34e912447fb0e0c4
Justin Kunimune <justinku@castor.subaru.nao.ac.jp>
2016-07-06 10:09:12 -1000

    Created a section for step 2, though it doesn't currently do anything.

--- MESOffset1.py
+++ MESOffset1.py
@@ -35,7 +35,6 @@ class MESOffset1(GingaPlugin.LocalPlugin):
         self.colors = ('green','red','blue','yellow','magenta','cyan','orange')
 
         # and some attributes
-        self.step = 1
         self.click_history = []
         self.click_index = -1
 
@@ -62,6 +61,8 @@ class MESOffset1(GingaPlugin.LocalPlugin):
         for i in range(len(self.star_list)):
             viewer = Viewers.CanvasView(logger=self.logger)
             viewer.set_desired_size(400,400)
+            viewer.enable_autozoom('on')
+            viewer.enable_autocuts('on')
             self.thumbnails.append(viewer)
         
         # now set up the ginga.canvas.types.layer.DrawingCanvas self.canvas,
@@ -70,12 +71,19 @@ class MESOffset1(GingaPlugin.LocalPlugin):
         self.canvas = self.dc.DrawingCanvas()
         self.canvas.enable_draw(False)
         self.canvas.set_callback('cursor-down', self.click_cb)  # left-click callback
-        self.canvas.set_callback('draw-down', lambda w,x,y,z: self.close()) # right-click callback
+        self.canvas.set_callback('draw-down', lambda w,x,y,z: self.next_cb(w)) # right-click callback
         self.canvas.set_surface(self.fitsimage)
         self.canvas.register_for_cursor_drawing(self.fitsimage)
         self.canvas.name = 'MOSA-canvas'
 
 
+    def next_cb(self, w):
+        """
+        Responds to the next button by proceeding to the next step
+        """
+        self.stack.set_index(self.stack.get_index()+1)
+        self.fv.showStatus("Crop each star image by clicking and dragging")
+    
     def click_cb(self, canvas, event, x, y):
         """
         Responds to a click on the screen
@@ -96,7 +104,7 @@ class MESOffset1(GingaPlugin.LocalPlugin):
         self.select_point(self.click_history[self.click_index])
             
             
-    def undo_cb(self):
+    def undo_cb(self, w):
         """
         Responds to the undo button by going back one click (if possible)
         """
@@ -106,7 +114,7 @@ class MESOffset1(GingaPlugin.LocalPlugin):
             self.select_point(self.click_history[self.click_index])
     
     
-    def redo_cb(self):
+    def redo_cb(self, w):
         """
         Responds to the redo button by going forward one click (if possible)
         """
@@ -119,7 +127,7 @@ class MESOffset1(GingaPlugin.LocalPlugin):
         """
         Sets a point as the current location of star #1,
         draws squares where it thinks all the stars are accordingly,
-        and updates all thumbnails.
+        and updates all thumbnails
         @param point:
             An int tuple containing the location of star #1
         """
@@ -133,10 +141,11 @@ class MESOffset1(GingaPlugin.LocalPlugin):
         
             # first, draw squares and numbers
             self.canvas.add(self.dc.CompoundObject(
-                                self.dc.SquareBox(x+dx-self.sq_size+1, y+dy,
+                                self.dc.SquareBox(x+dx, y+dy,
                                                   self.sq_size,
                                                   color=color),
-                                self.dc.Text(x+dx, y+dy, str(i+1),
+                                self.dc.Text(x+dx+self.sq_size+1, y+dy,
+                                             str(i+1),
                                              color=color)))
 
             # then, update the little pictures
@@ -148,85 +157,130 @@ class MESOffset1(GingaPlugin.LocalPlugin):
                                            ['transforms','cutlevels','rgbmap'])
             
         self.canvas.enable_draw(False)
+        
+        
+    def make_gui1(self, orientation='vertical'):
+        """
+        Constructs a GUI for the first step: locating the stars
+        @param orientation:
+            Either 'vertical' or 'horizontal', the orientation of this new GUI
+        @returns:
+            A Widgets.Box object containing all necessary buttons, labels, etc.
+        """
+        # start by creating the container
+        gui = Widgets.Box(orientation=orientation)
+        
+        # create a label to title this step
+        lbl = Widgets.Label("Step 1")
+        lbl.set_font(self.title_font)
+        gui.add_widget(lbl)
+
+        # fill a text box with brief instructions and put in in an expander
+        exp = Widgets.Expander(title="Instructions")
+        gui.add_widget(exp)
+        txt = Widgets.TextArea(wrap=True, editable=False)
+        txt.set_font(self.body_font)
+        txt.set_text("Left click on the star labeled '#1'. The other stars "+
+                     "should appear in the boxes below. Click again to select "+
+                     "another position. Click 'Next' when you are satisfied "+
+                     "with your location.")
+        exp.set_widget(txt)
+
+        # create a box to group the control buttons together
+        box = Widgets.HBox()
+        box.set_spacing(3)
+        gui.add_widget(box)
+
+        # the undo button goes back a step
+        btn = Widgets.Button("Undo")
+        btn.add_callback('activated', self.undo_cb)
+        btn.set_tooltip("Undo a single click (if a click took place)")
+        box.add_widget(btn)
+
+        # the redo button goes forward
+        btn = Widgets.Button("Redo")
+        btn.add_callback('activated', self.redo_cb)
+        btn.set_tooltip("Undo an undo action (if an undo action took place)")
+        box.add_widget(btn)
+
+        # the clear button erases the canvas
+        btn = Widgets.Button("Clear")
+        btn.add_callback('activated', lambda w: self.canvas.delete_all_objects())
+        btn.set_tooltip("Erase all marks on the canvas")
+        box.add_widget(btn)
+        
+        # the next button moves on to step 2
+        btn = Widgets.Button("Next")
+        btn.add_callback('activated', self.next_cb)
+        btn.set_tooltip("Accept and proceed to step 2")
+        box.add_widget(btn)
+        
+        # lastly, we need the zoomed-in images. This is the grid we put them in
+        num_img = len(self.star_list)   # total number of alignment stars
+        columns = 2                       # pictures in each row
+        rows = int(math.ceil(float(num_img)/columns))
+        grd = Widgets.GridBox(rows=rows, columns=columns)
+        gui.add_widget(grd)
+        
+        # these are the images we put in the grid
+        for row in range(0, rows):
+            for col in range(0, columns):
+                i = row*columns + col
+                if i < num_img:
+                    pic = Viewers.GingaViewerWidget(viewer=self.thumbnails[i])
+                    grd.add_widget(pic, row, col)
+        
+        return gui
+        
+        
+    def make_gui2(self, orientation='vertical'):
+        """
+        Constructs a GUI for the second step: cropping the stars
+        @param orientation:
+            Either 'vertical' or 'horizontal', the orientation of this new GUI
+        @returns:
+            A Widgets.Box object containing all necessary buttons, labels, etc.
+        """
+        # start by creating the container
+        gui = Widgets.Box(orientation=orientation)
+        
+        # create a label to title this step
+        lbl = Widgets.Label("Step 2")
+        lbl.set_font(self.title_font)
+        gui.add_widget(lbl)
+
+        # fill a text box with brief instructions and put in in an expander
+        exp = Widgets.Expander(title="Instructions")
+        gui.add_widget(exp)
+        txt = Widgets.TextArea(wrap=True, editable=False)
+        txt.set_font(self.body_font)
+        txt.set_text("Click and drag to include or exclude regions; "+
+                     "left-click will crop to selection, right or "+
+                     "middle-click will omit. Click 'Next' below to proceed "+
+                     "to the next star.")
+        exp.set_widget(txt)
+        
+        return gui
 
 
     def build_gui(self, container):
         """
-        Called when the plugin is invoked; setus up all the components of the GUI
+        Called when the plugin is invoked; sets up all the components of the GUI
         @param container:
             The widget.VBox this GUI will be added into
         """
-        # create the outer VBox that will hold everything else
-        out = Widgets.VBox()
+        # create the outer Box that will hold the stack of GUIs and close button
+        out, out_wrapper, orientation = Widgets.get_oriented_box(container)
         out.set_border_width(4)
-        container.add_widget(out)
-            
+        out.set_spacing
+        container.add_widget(out_wrapper)
+        
         # the rest depends on which step we are on
-        if self.step == 1:
-            # now create the VBox to hold the majority of the commands,
-            # the ScrollWidget that contains that box,
-            # and a string to specify whether this is horizontal or vertical
-            gui, gui_wrapper, orientation = Widgets.get_oriented_box(container)
-            gui.set_border_width(4)
-            gui.set_spacing(3)
-            out.add_widget(gui_wrapper)
-            
-            # create a label to title this step
-            lbl = Widgets.Label("Step 1")
-            lbl.set_font(self.title_font)
-            gui.add_widget(lbl)
-
-            # fill a text box with brief instructions and put in in an expander
-            exp = Widgets.Expander(title="Instructions")
-            gui.add_widget(exp)
-            txt = Widgets.TextArea(wrap=True, editable=False)
-            txt.set_font(self.body_font)
-            txt.set_text("Left click on the star labeled '#1'. The other stars should appear in the boxes below. Click again to select another position. Click 'Next' when you are satisfied with your location.")
-            exp.set_widget(txt)
-
-            # create a box to group the controls together
-            box = Widgets.HBox()
-            box.set_spacing(3)
-            gui.add_widget(box)
-
-            # the undo button goes back a step
-            btn = Widgets.Button("Undo")
-            btn.add_callback('activated', lambda w: self.undo_cb())
-            btn.set_tooltip("Undo a single click (if a click took place)")
-            box.add_widget(btn)
-
-            # the redo button goes forward
-            btn = Widgets.Button("Redo")
-            btn.add_callback('activated', lambda w: self.redo_cb())
-            btn.set_tooltip("Undo an undo action (if an undo action took place)")
-            box.add_widget(btn)
-
-            # the clear button erases the canvas
-            btn = Widgets.Button("Clear")
-            btn.add_callback('activated', lambda w: self.canvas.delete_all_objects())
-            btn.set_tooltip("Erase all marks on the canvas")
-            box.add_widget(btn)
-            
-            # the next button moves on to step 2
-            btn = Widgets.Button("Next")
-            btn.add_callback('activated', lambda w: self.close())
-            btn.set_tooltip("Accept and proceed to step 2")
-            box.add_widget(btn)
-            
-            # lastly, we need the zoomed-in images. This is the grid we put them in
-            num_img = len(self.star_list)   # total number of alignment stars
-            columns = 2                       # pictures in each row
-            rows = int(math.ceil(float(num_img)/columns))
-            grd = Widgets.GridBox(rows=rows, columns=columns)
-            gui.add_widget(grd)
-            
-            # these are the images we put in the grid
-            for row in range(0, rows):
-                for col in range(0, columns):
-                    i = row*columns + col
-                    if i < num_img:
-                        pic = Viewers.GingaViewerWidget(viewer=self.thumbnails[i])
-                        grd.add_widget(pic, row, col)
+        stk = Widgets.StackWidget()
+        stk.add_widget(self.make_gui1(orientation))
+        stk.add_widget(self.make_gui2(orientation))
+        out.add_widget(stk)
+        self.stack = stk    # this stack is important, so save it for later
 
         # end is an HBox that comes at the very end, after the rest of the gui
         end = Widgets.HBox()
@@ -264,9 +318,8 @@ class MESOffset1(GingaPlugin.LocalPlugin):
         self.canvas.delete_all_objects()
         
         # automatically select the first point and start
-        #if len(self.click_history) == 0:
-        self.click_cb(None, None, *self.star0)
         self.resume()
+        self.click_cb(None, None, *self.star0)
 
 
     def pause(self):
@@ -280,6 +333,7 @@ class MESOffset1(GingaPlugin.LocalPlugin):
         """
         Called when the plugin is refocused
         """
+        # activate the GUI
         self.canvas.ui_setActive(True)
         self.fv.showStatus("Locate the star labeled '1' by clicking.")
 
